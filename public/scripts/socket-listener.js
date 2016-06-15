62a8e78cf0d71a7a1ed41a1fdc5c86d138fbea97
Ernesto Hegi <ernesto.hegi@1000heads.com>
2016-06-15 18:07:02 +0100

    Cleans the code and updates styles.

--- public/scripts/socket-listener.js
+++ public/scripts/socket-listener.js
@@ -4,6 +4,7 @@ socket.on('tweet', function (tweet) {
     var container = document.querySelector('.twitter-container'),
         tweetContainer = document.createElement('div');
 
+    tweetContainer.className = 'tweet-container';
     tweetContainer.innerHTML = tweet.id + ' ' + tweet.user.screen_name + ' ' + tweet.text;
 
     container.insertBefore(tweetContainer, container.firstChild);
